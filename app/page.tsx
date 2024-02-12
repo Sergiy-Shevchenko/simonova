0a28a3a9e99c4f60916ebebd05eb7fec5b7e70a3
Sergiy-Shevchenko <116056626+Sergiy-Shevchenko@users.noreply.github.com>
2024-02-13 01:16:45 +0200

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+
 import { Hero } from '@/components/Hero/Hero'
 
 export default function Home() {
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { Lora } from "next/font/google"
+
 
 
 import css from './Header.module.css'
