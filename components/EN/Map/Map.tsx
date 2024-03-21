6e3683835f785d1ca8381c1402ccad0706ce3614
Sergiy-Shevchenko <116056626+Sergiy-Shevchenko@users.noreply.github.com>
2024-03-21 21:28:38 +0200

    update En map

--- components/EN/Map/Map.tsx
+++ components/EN/Map/Map.tsx
@@ -9,7 +9,7 @@ const Map = () => {
         <h3 className={styles.title}>Google map</h3>
       </div>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1512.2655628153605!2d30.50917905548956!3d50.39792245250369!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c94b47d3a02f%3A0xa0a5d4afb1d840c3!2sNotarius%20Simonova%20Viktoriya%20Valeriyivna!5e0!3m2!1sru!2sua!4v1709158299239!5m2!1sru!2sua"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5086.618995605129!2d30.507330514314837!3d50.39806938726347!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c94b47d3a02f%3A0xa0a5d4afb1d840c3!2sNotarius%20Simonova%20Viktoriya%20Valeriyivna!5e0!3m2!1sen!2sua!4v1711049166359!5m2!1sen!2sua"
         width="600"
         height="450"
         loading="lazy"
--- components/UA/Map/Map.tsx
+++ components/UA/Map/Map.tsx
@@ -9,12 +9,12 @@ const Map = () => {
         <h3 className={styles.title}>Google мапа</h3>
       </div>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1512.2655628153605!2d30.50917905548956!3d50.39792245250369!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c94b47d3a02f%3A0xa0a5d4afb1d840c3!2sNotarius%20Simonova%20Viktoriya%20Valeriyivna!5e0!3m2!1sru!2sua!4v1709158299239!5m2!1sru!2sua"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5086.6403320892505!2d30.505885859246824!3d50.39787055127343!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4c94b47d3a02f%3A0xa0a5d4afb1d840c3!2z0J3QvtGC0LDRgNGW0YPRgSDQodGW0LzQvtC90L7QstCwINCS0ZbQutGC0L7RgNGW0Y8g0JLQsNC70LXRgNGW0ZfQstC90LA!5e0!3m2!1suk!2sua!4v1711048846314!5m2!1suk!2sua" 
         width="600"
         height="450"
         loading="lazy"
         className={styles.map}
-      ></iframe>
+      ></iframe>      
     </section>
   );
 };
